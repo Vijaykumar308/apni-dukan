4f29807e2ac9fbe8a156fd064e56cfc3b3598c37
Vijay Kumar <308vijaykumar@gmail.com>
2025-04-11 00:04:41 +0530

    project listed on ui

--- src/app/products/@ProductCard/page.jsx
+++ src/app/products/@ProductCard/page.jsx
@@ -13,38 +13,42 @@ async function productCard() {
   console.log(data);
 
   return (
+    <div className='grid gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5'>
+      {
+        data.map((item, index) => (
+          <div key={index} className='card flex flex-col pb-5 rounded-md' style={{boxShadow: "rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px"}}>
+            <div className='card_image flex justify-center items-center'>
+              <Image src={item?.images.shift()} alt='addfds' width={200} height={200} />
+            </div>
 
-    data.map((item, index) => (
+            <div className='px-3'>
+              <div className='py-3 text-md text-gray-600 font-semibold'>
+                <p>{item?.title}</p>
+                <p className='mt-4 font-light text-gray-600'>{item?.description?.length > 100
+    ? `${item.description.slice(0, 100)}...`
+    : item?.description}</p>
+              </div>
 
-      <div key={index} className='card flex flex-col max-w-[16%]'>
-        <div className='card_image flex justify-center items-center'>
-          <Image className='w-1/3' src={item?.images.pop()} alt='addfds' width={200} height={200} />
-        </div>
+              <div className='flex justify-between items-center pt-3'>
+                <p className='price'><span className='text-2xl text-[#6477f2] font-bold mr-2 text-shadow-amber-950'>${item?.price}</span><span className='text-sm line-through'>219.99</span></p>
+                <span className='bg-green-400 text-green-800 text-sm px-2 text-white'>-50%</span>
+              </div>
 
-        <div className='px-3'>
-          <div className='py-3 text-md text-gray-600 font-semibold'>
-            <p>Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops</p>
-          </div>
+              <p className='text-xs text-gray-500 py-1'>Eligible for free shipping</p>
+              <div className='rating py-3'>
+                <div className='flex'>
+                  <p className='flex space-x-1 items-center'>
+                    {Array(4).fill().map((_, index) => <Star key={index} fill='orange' stroke='orange' size={15} />)} <span className='text-sm ml-3'>4</span>
+                  </p>
+                </div>
+              </div>
 
-          <div className='flex justify-between items-center pt-3'>
-            <p className='price'><span className='text-2xl text-[#6477f2] font-bold mr-2 text-shadow-amber-950'>$109.95</span><span className='text-sm line-through'>219.99</span></p>
-            <span className='bg-green-400 text-green-800 text-sm px-2 text-white'>-50%</span>
-          </div>
-
-          <p className='text-xs text-gray-500 py-1'>Eligible for free shipping</p>
-          <div className='rating py-3'>
-            <div className='flex'>
-              <p className='flex space-x-1 items-center'>
-                {Array(4).fill().map((_, index) => <Star key={index} fill='orange' stroke='orange' size={15} />)} <span className='text-sm ml-3'>4</span>
-              </p>
+              <button className='bg-orange-600 py-2 px-2 rounded-sm text-sm text-white w-full flex justify-center cursor-pointer'> <span className='mr-3'> <ShoppingCart /></span> Add to Cart</button>
             </div>
           </div>
-
-          <button className='bg-orange-600 py-2 px-2 rounded-sm text-sm text-white w-full flex justify-center cursor-pointer'> <span className='mr-3'> <ShoppingCart /></span> Add to Cart</button>
-        </div>
-      </div>
-
-    ))
+        ))
+      }
+    </div>
   )
 
 }
